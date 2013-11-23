b14eea4233c5dfe7cf6e4a7a60a248f25cffbc42
Jason El-Massih <jel-massih@hotmail.com>
2013-11-23 02:57:03 -0500

    Added Creating Races

--- inc/createRace.php
+++ inc/createRace.php
@@ -3,26 +3,29 @@ include('config.php');
 
 if(isset($_POST['event_name']) && isset($_POST['location']) && isset($_POST['prize'])) {
 
-	$checkpoints = json_decode($_POST['checkpoints']);
+	$checkpoints = json_decode(stripslashes($_POST['checkpoints']));
 	$goodcp = array();
 	foreach($checkpoints as $checkpoint) {
 		$checkpoint->clue = urldecode($checkpoint->clue);
 		array_push($goodcp, $checkpoint);
 	}
 
-	$doc = array(
-		"event_name" => $_POST['event_name'],
-		"location" => $_POST['location'],
-		"prize" => $_POST['prize'],
-		"participants" => array(),
-		"checkpoints" => $goodcp,
-		"currentRanking" => 1,
-		"winner" => ""
-	);
+	$doc = new stdClass();
+	$doc->_id = $_POST['event_name'];
+	$doc->location = $_POST['location'];
+	$doc->prize = $_POST['prize'];
+	$doc->participants = array();
+	$doc->checkpoints = $goodcp;
+	$doc->currentRanking = 1;
+	$doc->winner = "";
 
-	global $collection;
-	$collection->insert($doc);
-	header("Location: launchGame.html?event_name=".$_POST['event_name']);
+	try {
+		$response = $client->storeDoc($doc);
+		header("Location: ../launchGame.html?event_name=".$_POST['event_name']);
+	} catch(Exception $e) {
+		echo("Error! ".$e->getMessage());
+	}
+
 
 } else {
 	die("FILL IN THE FIELDS YOU FOOL!");
