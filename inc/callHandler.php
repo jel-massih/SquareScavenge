782b87a0432b8aad483749412befa63fc6a683a1
Jason El-Massih <jel-massih@hotmail.com>
2013-11-23 20:06:23 -0500

    Fixed Race launch. Now calls and texts everyone

--- inc/callHandler.php
+++ inc/callHandler.php
@@ -8,11 +8,11 @@
 	$client = new Services_Twilio($AccountSid, $AuthToken);
 	$_GET['clue'] = urlencode($_GET['clue']);
 	try {
-		$call = $client->account->calls->create($TwilioNumber, $_GET['phonenumber'], 'http://jel-massih.com/TestingServer/LocTask/calltext.php?clue='.$_GET['clue']);
+		$call = $client->account->calls->create($TwilioNumber, $_GET['phonenumber'], 'http://jel-massih.com/SquareScavenge/inc/calltext.php?clue='.$_GET['clue']);
 	} catch (Exception $e) {
 	}
 	
 	$_GET['clue'] = urldecode($_GET['clue']);
 
-	$sms = $client->account->sms_messages->create($TwilioNumber, $_GET['phonenumber'],"Next Clue: ".urldecode($_GET['clue']), array());
+	$sms = $client->account->sms_messages->create($TwilioNumber, $_GET['phonenumber'],"Next Clue: ".$_GET['clue'], array());
 ?>
--- inc/launchRace.php
+++ inc/launchRace.php
@@ -1,16 +1,15 @@
 <?php
 	include('config.php');
 	
-	global $collection;
- 	$race = $collection->findOne(array('event_name' => $_GET['event_name']));
+ 	$doc = $client->getDoc($_GET['event_name']);
 
- 	foreach($race['participants'] as $participant) {
- 		giveFirstClue($participant['phonenumber'], $race['checkpoints'][0]['clue']);
+ 	foreach($doc->participants as $participant) {
+ 		giveFirstClue($participant->phonenumber, $doc->checkpoints[0]->clue);
  	}
 
  	function giveFirstClue($number, $clue) {
  		$clue = urlencode($clue);
-		file_get_contents("http://jel-massih.com/TestingServer/LocTask/callHandler.php?phonenumber=".$number."&clue=".$clue);
-		header("Location: index.html");
+		file_get_contents("http://jel-massih.com/SquareScavenge/inc/callHandler.php?phonenumber=".$number."&clue=".$clue);
+		header("Location: ../index.html");
  	}
 ?>
